75840cae08f980a72d84b0b9f1fe23bb1b467e46
wal <walter.mcconnell@gmail.com>
2013-02-13 23:39:32 +0000

    initial

---
+++ 3_Largest_Prime_Factor.rb
@@ -0,0 +1,57 @@
+TESTS = [[13195,29], [600851475143, 6857]]
+
+def prime?(number)
+  
+  if number < 2
+    return false
+  else
+    
+    (2...Math.sqrt(number)).each do |n|
+      return false if (number % n).zero?
+    end
+    
+  end
+  
+  true
+end
+
+def factors(number)
+  factors = [1, number]
+  
+  (2..Math.sqrt(number)).each do |n|
+    factors << n if (number % n).zero?
+  end
+  
+  factors.sort
+end
+
+def prime_factors(number)
+  factors(number).keep_if{|n| prime?(n)}
+end
+
+def largest_prime_factor(number)
+  prime_factors(number).max
+end
+
+
+require 'test/unit'
+class LargestPrimeFactorTest < Test::Unit::TestCase
+
+  def test_largest_prime_factors
+    TESTS.each do |test_details|
+      start_time = Time.now
+      assert_equal test_details[1], largest_prime_factor(test_details[0])
+      puts "Time for #{test_details[0]} : #{Time.now - start_time}"
+    end
+  end
+
+  def test_prime_factors
+    assert_equal [5,7,13,29], prime_factors(TESTS[0][0])
+  end
+
+  def test_prime
+    assert !prime?(1)
+    assert prime?(5)
+    assert !prime?(8)
+  end
+end
---
+++ 4_largest_palindrome_product.rb
@@ -0,0 +1,48 @@
+def largest_palindrome(start_range, end_range)
+  palindromes = []
+
+  (start_range..end_range).each do |temp_start|
+    (temp_start..end_range).each do |temp_end|
+      product = temp_start * temp_end
+      palindromes << product if is_palindrome?(product)
+    end
+  end
+  
+  palindromes.max
+end
+
+
+def is_palindrome?(number)
+  digits = []
+  
+  begin
+    digits << number % 10
+    number /= 10
+  end while number > 10
+
+  digits << number if number > 0
+
+  if digits.size > 1
+    (0..digits.size).each do |n|
+      return false unless digits[n].eql?(digits[(n *-1) -1])
+    end
+  else
+    return true
+  end
+end
+
+
+
+require 'test/unit'
+class LargestPalindromeProductTest < Test::Unit::TestCase
+  def test_is_palindrome
+    assert is_palindrome?(5)
+    assert is_palindrome?(313)
+    assert is_palindrome?(9009)
+    assert !is_palindrome?(1234)
+  end
+
+  def test_largest_palindrome
+    assert_equal 9009, largest_palindrome(100, 999)
+  end
+end
